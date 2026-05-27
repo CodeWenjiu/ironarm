f71dceda6248214732490b9ac6677e26e29e203e
wenjiu <2784307979@qq.com>
2026-05-27 10:26:11 +0800

    chore

--- ironarm_sim/src/main.rs
+++ ironarm_sim/src/main.rs
@@ -4,7 +4,7 @@ mod tasks;
 use bevy::app::{App, AppExit};
 use bevy::prelude::{
     DefaultPlugins, MessageReader, MessageWriter, PostUpdate,
-    Res, ResMut, Resource, Time, Update,
+    ResMut, Resource, Update,
 };
 use cu29::prelude::*;
 use cu29::simulation::{CuTaskCallbackState, SimOverride};
@@ -13,55 +13,42 @@ use ironarm_core::messages::JointCommand;
 #[copper_runtime(config = "copperconfig.ron", sim_mode = true)]
 struct IronArmSim {}
 
-fn sim_callback(step: crate::default::SimStep) -> SimOverride {
-    match step {
-        crate::default::SimStep::CmdSrc(CuTaskCallbackState::Process(_input, output)) => {
-            output.set_payload(JointCommand {
-                target_angle: 0.5,
-                target_velocity: 0.0,
-                stiffness: 1.0,
-            });
-            SimOverride::ExecutedBySim
-        }
-        crate::default::SimStep::StateSink(CuTaskCallbackState::Process(input, _output)) => {
-            let (j0, j1) = input;
-            if let Some(s) = j0.payload() {
-                eprintln!("Sink[sim][0]: angle={}", s.current_angle);
-            }
-            if let Some(s) = j1.payload() {
-                eprintln!("Sink[sim][1]: angle={}", s.current_angle);
-            }
-            SimOverride::ExecutedBySim
-        }
-        _ => SimOverride::ExecuteByRuntime,
-    }
+fn noop_callback(_step: crate::default::SimStep) -> SimOverride {
+    SimOverride::ExecuteByRuntime
+}
+
+fn set_msg_timing<T: CuMsgPayload>(clock: &RobotClock, msg: &mut CuMsg<T>) {
+    let perf = cu29::curuntime::perf_now(clock);
+    msg.tov = clock.now().into();
+    msg.metadata.process_time.start = perf.into();
+    msg.metadata.process_time.end = perf.into();
 }
 
 #[derive(Resource)]
 struct CopperApp {
     app: IronArmSim,
-    clock_mock: RobotClockMock,
+    clock: RobotClock,
 }
 
 fn main() {
     let logger_path = std::env::temp_dir().join("ironarm_sim.copper");
-    let (robot_clock, clock_mock) = RobotClock::mock();
+    let clock = RobotClock::default();
 
     let mut copper = IronArmSim::builder()
-        .with_clock(robot_clock.clone())
+        .with_clock(clock.clone())
         .with_log_path(&logger_path, Some(1024 * 1024 * 10))
         .expect("Failed to setup logger.")
-        .with_sim_callback(&mut sim_callback)
+        .with_sim_callback(&mut noop_callback)
         .build()
         .expect("Failed to create sim runtime.");
 
     copper
-        .start_all_tasks(&mut sim_callback)
+        .start_all_tasks(&mut noop_callback)
         .expect("Failed to start all tasks.");
 
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
-    app.insert_resource(CopperApp { app: copper, clock_mock });
+    app.insert_resource(CopperApp { app: copper, clock });
     app.add_systems(Update, run_tick);
     app.add_systems(PostUpdate, stop_copper_on_exit);
     app.run();
@@ -69,11 +56,36 @@ fn main() {
 
 fn run_tick(
     mut copper: ResMut<CopperApp>,
-    time: Res<Time>,
     mut exit_writer: MessageWriter<AppExit>,
 ) {
-    copper.clock_mock.set_value(time.elapsed().as_nanos() as u64);
-    if let Err(e) = copper.app.run_one_iteration(&mut sim_callback) {
+    let clock = copper.clock.clone();
+    let mut cb = move |step: crate::default::SimStep| -> SimOverride {
+        match step {
+            crate::default::SimStep::CmdSrc(CuTaskCallbackState::Process(_input, output)) => {
+                output.set_payload(JointCommand {
+                    target_angle: 0.5,
+                    target_velocity: 0.0,
+                    stiffness: 1.0,
+                });
+                set_msg_timing(&clock, output);
+                SimOverride::ExecutedBySim
+            }
+            crate::default::SimStep::StateSink(CuTaskCallbackState::Process(input, output)) => {
+                let (j0, j1) = input;
+                if let Some(s) = j0.payload() {
+                    debug!("Sink[sim][0]: angle={:.3} rad", s.current_angle);
+                }
+                if let Some(s) = j1.payload() {
+                    debug!("Sink[sim][1]: angle={:.3} rad", s.current_angle);
+                }
+                set_msg_timing(&clock, output);
+                SimOverride::ExecutedBySim
+            }
+            _ => SimOverride::ExecuteByRuntime,
+        }
+    };
+
+    if let Err(e) = copper.app.run_one_iteration(&mut cb) {
         eprintln!("Sim stopped: {}", e);
         exit_writer.write(AppExit::Success);
     }
@@ -86,7 +98,7 @@ fn stop_copper_on_exit(
     if exit_events.read().next().is_some() {
         copper
             .app
-            .stop_all_tasks(&mut sim_callback)
+            .stop_all_tasks(&mut noop_callback)
             .expect("Failed to stop all tasks.");
     }
 }
